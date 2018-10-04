bc4036e4fc995308e6aa05ed4b5ab287ee51a7cc
Teresa Plaz <teresaplaz@gmail.com>
2018-10-04 02:14:24 -0400

    steps solved but ugh

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,54 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps ( n, row = 0, stair = "" )
+{
+  //Solution #1 i hate double loops but whatevs
+
+  // for ( let row = 0; row < n; row++ )
+  // {
+  //   let stair = "";
+  //   for ( let column = 0; column < n; column++ )
+  //   {
+  //     if ( column <= row )
+  //     {
+  //       stair += "#";    
+  //     }
+  //     else
+  //     {
+  //       stair += " ";  
+  //     }
+  //   }
+
+  //   console.log( stair );
+  // }
+
+
+  //Solution #2 Recursion
+
+
+  if ( n === row )
+  {
+    return;
+  }
+
+  if ( n === stair.length )
+  {
+    console.log( stair );
+    steps( n, row + 1 );
+    return;
+  }
+
+  if ( stair.length <= row )
+  {
+    stair += "#";
+  }
+  else
+  {
+    stair += " ";
+  }
+
+  steps( n, row, stair );
+}
 
 module.exports = steps;
