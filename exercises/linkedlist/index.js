5fc08a0a99c3ef7891d849135da23cef19f6b299
Teresa Plaz <teresaplaz@gmail.com>
2018-10-17 00:49:50 -0400

    linked lists 7/29

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -15,12 +15,47 @@ class LinkedList
 {
   constructor ()
   {
-    this.head = null; 
+    this.head = null;
   }
 
   insertFirst (data)
   {
-    this.head = new Node(data, this.head);
+    this.head = new Node( data, this.head );
+  }
+
+  size ()
+  {
+    let counter = 0;
+    let node = this.head;
+    {
+      while (node)
+      {
+        counter++;
+        node = node.next;
+      }
+    }
+    return counter;
+  }
+
+  getFirst ()
+  {
+    return this.head;
+  }
+
+  getLast ()
+  {
+    let node = this.head;
+
+    if ( node.next )
+    {
+      while ( node )
+      {
+        node = node.next;
+        console.log( node );
+      }
+    }
+    console.log( node );
+      return node;
   }
 }
 
