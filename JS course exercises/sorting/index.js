f299490834557fb7f5a05c672acc633f4805ee89
Teresa Plaz <teresaplaz@gmail.com>
2018-11-03 20:04:42 -0400

    bubble sort

--- JS course exercises/sorting/index.js
+++ JS course exercises/sorting/index.js
@@ -3,6 +3,19 @@
 
 function bubbleSort(arr) {
 
+  for ( let i = 0; i < arr.length; i++ )
+  {
+    for ( let j = 0; j < arr.length - i; j++ )
+    {
+      if ( arr[ j ] > arr[ j + 1 ] )
+      {
+        const tempGreater = arr[ j ]; 
+        arr[ j ] = arr[ j + 1 ];
+        arr[ j + 1 ] = tempGreater;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
